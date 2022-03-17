7dce4fab3667c55d6460a014b40c36e3c913033c
Joao Pedro <joaopedro@sobremail.com>
2022-03-16 22:34:50 -0300

    function for scrapping explore created

--- web/instagram.js
+++ web/instagram.js
@@ -10,8 +10,10 @@ async function createBrowserInstance() {
     const browser = await puppeteer.launch({ headless: false });
     websocketURL = browser.wsEndpoint()
     const page = (await browser.pages())[0]
-    await page.goto('https://www.instagram.com/');
 
+    page.on('console', msg => console.log('browser console: ', msg.text()))
+
+    await page.goto('https://www.instagram.com/');
     await page.waitForSelector('[name="username"]');
     await page.type('[name="username"]', process.env.LOGIN_EMAIL_INSTAGRAM);
     await page.type('[type="password"]', process.env.LOGIN_PASSWORD_INSTAGRAM);
@@ -24,13 +26,12 @@ async function puppInstagram(func) {
 
     const browser = await puppeteer.connect({ browserWSEndpoint: websocketURL })
     const page = (await browser.pages())[0]
-    await page.goto('https://www.instagram.com/')
 
     async function checkIfUserExists(user) {
         let returned = await page.goto(`https://www.instagram.com/${user}`).then(async res => {
             await page.waitForSelector('body')
             const errorBody = await page.$('.p-error.dialog-404')
-            if(res.status === 404 || errorBody) return false
+            if (res.status === 404 || errorBody) return false
 
             return true
         })
@@ -48,26 +49,74 @@ async function puppInstagram(func) {
         if (noResultsDiv) return 'Invalid Username'
 
         await page.waitForSelector('.-qQT3') //<a> tags with the user profiles links on search bar
-        const possibleUsersNames = await page.$$eval('.-qQT3' , searchedResults => {
+        const possibleUsersNames = await page.$$eval('.-qQT3', searchedResults => {
 
             const usersList = searchedResults.filter(item => !item.href.includes('/explore/'))
             const possibleUsers = usersList.filter((item, i) => i <= 10)
-            return possibleUsers.map(item => item.children[0].children[1].children[0].children[0].children[0].children[0].innerHTML)
+            return possibleUsers.map(item => item.firstChild.children[1].firstChild.firstChild.firstChild.firstChild.innerHTML)
 
         })
         return possibleUsersNames
     }
 
-    async function explore(){
+    async function exploreImages() {
 
         await page.waitForSelector('[href="/explore/"]')
         await page.click('[href="/explore/"]')
 
-        const posts = page.$$('')
+        await page.waitForSelector('div.QzzMF.Igw0E.IwRSH.eGOV_._4EzTm.NUiEW')
+        await page.waitForTimeout(1500)
 
+        const postsJSHandle = await page.$$('div.QzzMF.Igw0E.IwRSH.eGOV_._4EzTm.NUiEW')
+        await page.exposeFunction('getImagesSlides', async postIndex => {
+            let post = postsJSHandle[postIndex]
+            console.log('getImagesSlides sendo chamada')
+            await page.evaluateHandle( post => post.firstChild.click(), post)
+            console.log('passou do image link')
+            await page.waitForSelector('button[aria-label="Avançar"]').then(async button => await button.click())
+            console.log('passou daq')
+
+            const postImageCollection = await page.$$eval('li.Ckrof', async imagesSlides => {
+                let postImageCollection = []
+                for (let imageSlide of imagesSlides){
+                    console.log('for do getImageSlides')
+                    postImageCollection.push(imageSlide.firstChild.firstChild.firstChild.firstChild.firstChild.src) 
+                }
+                return postImageCollection
+            })
+
+            await page.goBack()
+            console.log('passou do for', postImageCollection)
+            return postImageCollection
+        })
+        const images = await page.$$eval('div.QzzMF.Igw0E.IwRSH.eGOV_._4EzTm.NUiEW' ,async posts => {
+            let images = []
+            console.log('chegou no eval')
+
+            for ( let i = 0; i < posts.length; i++) {
+                const post = posts[i]
+                console.log('chegou no for')
+                //weather post has a svg on top left corner
+                if (post.firstChild.children[1]) {
+                    console.log('chegou no if que verifica svg')
+                    if (post.firstChild.children[1].firstChild.ariaLabel === 'Carrossel'){
+                        console.log('chegou no if do getImagesSlides')
+                        await getImagesSlides(i).then(imagesSlides => images.push(imagesSlides))
+                    } else {
+                        console.log(post.firstChild.children[1].firstChild.ariaLabel)
+                    }
+
+                } else{ images.push(post.firstChild.firstChild.firstChild.firstChild.src)
+                    console.log('chegou no else')}
+            }
+            return images
+        })
+
+        await page.goBack()
+        return images
     }
 
-    return func({ checkIfUserExists, searchForUser, explore })
+    return func({ checkIfUserExists, searchForUser, exploreImages })
 }
 
 async function getUserInstagramInfo(user) {
@@ -106,9 +155,13 @@ async function getUserInstagramInfo(user) {
     return userData
 }
 
-async function scrappInstagramTag(tag){
-    
+async function scrappInstagramTag(tag) {
+
 }
+
+puppInstagram(async ({ exploreImages, searchForUser }) => {
+    console.log(await exploreImages())
+})
 
 module.exports = {
     puppInstagram,
